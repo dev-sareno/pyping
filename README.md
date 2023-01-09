cab127b2772d83b5baac2be89c6d0913e24bd7b8
Eladio Sareno Jr <eladio.sareno@netzontech.com>
2023-01-09 17:43:36 +0800

    chore(init): add init files

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # pyping
 Python simple program that connect and call simple query to a Postgres database; useful for testing your application if it can reach the database
+
+## Environment
+```shell
+$ pyenv local 3.8.13
+$ python -V
+Python 3.8.13
+$ python -m venv .venv
+$ source ./venv/bin/activate
+$ pip install pip --upgrade
+$ pip install requirements.txt
+```
+
+## Run
+```shell
+$ export APP_DB_CONNECTION_STRING="postgres://username:password@localhost:5432/mydb"
+$ export APP_LOOP_INTERVAL_SECONDS="3"
+$ python main.py
+```
---
+++ main.py
@@ -0,0 +1,68 @@
+import psycopg2
+import os, sys, time
+
+
+env_db_connection_string: str = "APP_DB_CONNECTION_STRING"
+env_loop_interval: str = "APP_LOOP_INTERVAL_SECONDS"
+
+
+def float_try_parse(value):
+    try:
+        return float(value), True
+    except ValueError:
+        return value, False
+
+
+def print_error(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+
+
+def main():
+    db_conn = os.environ.get(env_db_connection_string)
+    if db_conn is None:
+        print_error(f"env {env_db_connection_string} is required")
+        exit(1)
+    
+    loop_iterval_str = os.environ.get(env_loop_interval)
+    loop_iterval: int = 0
+    if loop_iterval_str is None:
+        print_error(f"env {env_loop_interval} is required")
+        exit(1)
+    else:
+        # validate int
+        val, valid = float_try_parse(loop_iterval_str)
+        if not valid:
+            print_error(f"env {env_loop_interval} must be a valid integer")
+            exit(1)
+        loop_iterval = val
+        
+
+    while True:
+        try:
+            # Connect to your postgres DB
+            conn = psycopg2.connect(db_conn)
+
+            # Open a cursor to perform database operations
+            cur = conn.cursor()
+
+            # Execute a query
+            cur.execute("SELECT NOW(), VERSION();")
+
+            row = cur.fetchone()
+            print(f"result:\n{row}")
+
+            # sleep
+            time.sleep(loop_iterval)
+        except Exception as e:
+            print(f"unhandled error: {e}")
+            continue
+        except KeyboardInterrupt:
+            print(f"cancelled by user")
+            break
+        finally:
+            if conn is not None:
+                conn.close()
+                print(f"db disconected")
+
+if __name__ == "__main__":
+    main()
